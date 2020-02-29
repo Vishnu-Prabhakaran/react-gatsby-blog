910cc886a075ad9923606c670c9c7ab8de7d552c
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-02-29 17:54:34 +1100

    configuring node to create slug

--- gatsby-blog/gatsby-node.js
+++ gatsby-blog/gatsby-node.js
@@ -5,3 +5,25 @@
  */
 
 // You can delete this file if you're not using it
+
+// Not react code
+
+// Gatsby source file system
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  // internal - property which describes the data about the node
+  console.log(node.internal.type)
+  // if its equal then create aslug
+  // a slug is a url link inside Gatsby that the browser is able to access
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+
+    createNodeField({
+        node,
+        name: `slug`,
+        value: slug
+    })
+  }
+}
