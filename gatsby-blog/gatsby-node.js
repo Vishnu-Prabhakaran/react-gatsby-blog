7256126d77483fc0530ed6c7ac6272e1e4b27322
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-02-29 22:25:37 +1100

    Creating dynamic slug pages

--- gatsby-blog/gatsby-node.js
+++ gatsby-blog/gatsby-node.js
@@ -10,7 +10,7 @@
 
 // Gatsby source file system
 const { createFilePath } = require(`gatsby-source-filesystem`)
-
+const path = require(`path`)
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   // internal - property which describes the data about the node
@@ -21,9 +21,37 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     const slug = createFilePath({ node, getNode })
 
     createNodeField({
-        node,
-        name: `slug`,
-        value: slug
+      node,
+      name: `slug`,
+      value: slug,
     })
   }
+}
+
+// Create Pages
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          slug: node.fields.slug,
+        },
+      })
+    })
+  })
 }
